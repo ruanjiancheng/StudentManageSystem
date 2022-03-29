4cd99e9edcf48e85ac2c3cf2a8bceedf74f97c4f
auggie <57210517+ruanjiancheng@users.noreply.github.com>
2022-03-29 21:06:39 +0800

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 service mysqld start
 cd student_client
 nohup npm run serve>student_client.log 2>&1 &
-cd ../student_server
-nohup java -jar student_server.jar>student_rver.log 2>&1 &
+cd ../student_serverse
+nohup java -jar student_server.jar>student_server.log 2>&1 &
