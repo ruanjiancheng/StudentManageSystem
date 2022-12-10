744dfed2530d52260a23f9cdb19e386216f12279
auggie <57210517+ruanjiancheng@users.noreply.github.com>
2022-12-10 12:41:46 +0800

    Update home.vue

--- student_client/src/views/Admin/home.vue
+++ student_client/src/views/Admin/home.vue
@@ -6,7 +6,7 @@
         <el-card style="width: 40%">
           <p><i class="el-icon-s-home" style="margin-right: 18px"></i>项目名：学生管理系统</p>
           <p><i class="el-icon-s-custom" style="margin-right: 18px"></i>开发人员：auggie</p>
-          <p><i class="el-icon-location-outline" style="margin-right: 18px"></i>上海大学</p>
+          <p><i class="el-icon-location-outline" style="margin-right: 18px"></i>HMU</p>
           <p><i class="el-icon-chat-round" style="margin-right: 18px"></i>QQ: 1434919953</p>
           <p><i class="el-icon-error" style="margin-right: 18px;"></i>仅供学习，禁止抄袭</p>
         </el-card>
